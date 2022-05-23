7390816a76c04454960f6825ee979e4a4c7489b3
Trevor Smith <tjs_uk@outlook.com>
2022-05-23 15:51:25 +0100

    Adding another form of polymorphism in the main class.  Using multiple classes for an arrayList.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         Cat myCat = new Cat("Garfield",8,"Ginger");
@@ -9,5 +11,19 @@ public class Main {
 
         var myCollie = new BorderCollie("Shep",9,"Black and white");
         System.out.println(myCollie.getName()+" has "+myCollie.getEyes());
+
+        //Creating a Pet Shop Inventory List of Dogs and Cats
+        ArrayList<Pet> petStore = new ArrayList<>();
+
+        //Now for another form of polymorphism, using multiple classes within a single list
+        //this only works when you are using sub-classes of a superclass.
+        petStore.add(myCat);
+        petStore.add(myDog);
+        petStore.add(myCollie);
+
+        System.out.println("\nMy pet store contains");
+        for(Pet p: petStore){
+            System.out.println(p.getName() + " is a " + p.getClass() + " and is " + p.getAge() + " years old.");
+        }
     }
 }
